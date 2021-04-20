b9c6abdf82fca2b8f41fc81fe466475153a64660
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-04-20 19:32:47 +0300

    Подключение отправки формы в телеграм

---
+++ send.php
@@ -0,0 +1,24 @@
+<?php
+
+$man = $_POST['user-man'];
+$woman = $_POST['user-woman'];
+$token = "1700373059:AAFbVaPpJ5rS6YTJCcs1wKRb02EXNCl-IeY";
+$chat_id = "-520974542";
+$arr = array(
+  'Гость: ' => $man,
+  'Гостья: ' => $woman
+);
+
+foreach ($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+}
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank_you.html');
+} else {
+  echo "Error";
+}
+?>
+
+https://api.telegram.org/bot1700373059:AAFbVaPpJ5rS6YTJCcs1wKRb02EXNCl-IeY/getUpdates
